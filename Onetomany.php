7d46bf9e70d2dca84033051f8b2533cab0c0261e
Lútsen Stellingwerff <lutsen@hoverkraft.nl>
2016-05-07 17:37:14 +0100

    First!

---
+++ Onetomany.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Lagan\Property;
+
+/**
+ * Controller for the Lagan one-to-many property.
+ * Lets the user define a one-to-many relation between two content objects.
+ * The Onetomany property type controller enables a one-to-many relation between 2 Lagan models.
+ * The name of the property should be the name of the Lagan model this model can have a one-to-many
+ * relation with. For this to work properly the other model should have a many-to-one relation with this model.
+ * So in our example in the Lagan project the LaganHoverkraft model has a one-to-many relation
+ * with the LaganCrew model, and the LaganCrew model has a many-to-one relation with the LaganHoverkraft model.
+ *
+ * A property type controller can contain a set, read, delete and options method. All methods are optional.
+ * To be used with Lagan: https://github.com/lutsen/lagan
+ */
+
+class Onetomany {
+
+	/**
+	 * The read method is executed each time a property with this type is read.
+	 *
+	 * @param bean		$bean		The Readbean bean object with this property.
+	 * @param array		$property	Lagan model property arrray.
+	 *
+	 * @return array	Array with Redbean beans with a many-to-one relation with the object with this property.
+	 */
+	public function read($bean, $property) {
+
+		$list_name = 'own'.ucfirst($property['name']).'List';
+		return  $bean->{ $list_name };
+
+	}
+
+	/**
+	 * The options method returns all the optional values this property can have.
+	 *
+	 * @param array		$property	Lagan model property arrray.
+	 *
+	 * @return array	Array with all beans of the $property['name'] Lagan model.
+	 */
+	public function options($property) {
+		return \R::findAll( $property['name'] );
+	}
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,13 @@
+[<img src="https://cdn.rawgit.com/lutsen/lagan/master/lagan-logo.svg" width="100" alt="Lagan">](https://github.com/lutsen/lagan)
+
+Lagan One-to-many Property
+==========================
+
+Controller for the Lagan one-to-many property.  
+Lets the user define a one-to-many relation between two content objects.
+
+The Onetomany property type controller enables a one-to-many relation between 2 Lagan models. The name of the property should be the name of the Lagan model this model can have a one-to-many relation with. For this to work properly the other model should have a many-to-one relation with this model. So in our example in the Lagan project the LaganHoverkraft model has a one-to-many relation with the LaganCrew model, and the LaganCrew model has a many-to-one relation with the LaganHoverkraft model.
+
+To be used with [Lagan](https://github.com/lutsen/lagan). Lagan lets you create flexible content objects with a simple class, and manage them with a web interface.
+
+Lagan is a project of [Lútsen Stellingwerff](http://lutsen.land/) from [HoverKraft](http://www.hoverkraft.nl/).
